d9c398f5d6bafaaa6395bd8eff39f48f0584d66e
Blizzard Finnegan <blizzardfinnegan@gmail.com>
2023-06-06 08:19:57 -0400

    Update git config, fix fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,7 @@
 gpgconf --launch gpg-agent
 set -e SSH_AUTH_SOCK
-set -gx SSH_AUTH_SOCK $(gpgconf --list-dirs agent-ssh-socket)
-set -gx GPG_TTY $(tty)
+set -gx SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+set -gx GPG_TTY (tty)
 gpg-connect-agent updatestartuptty /bye >/dev/null
 if status is-interactive
     # Commands to run in interactive sessions can go here
