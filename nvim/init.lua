765be0d043978596e1687c82ee765767ebc7e927
Blizzard Finnegan <49597944+blizzardfinnegan@users.noreply.github.com>
2025-05-28 15:11:13 -0600

    Remove error-causing extra text

--- nvim/init.lua
+++ nvim/init.lua
@@ -278,7 +278,7 @@ local on_attach = function(_, bufnr)
   end, '[W]orkspace [L]ist Folders')
 
   -- Create a command `:Format` local to the LSP buffer
-  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)diff lsp
+  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
     if vim.lsp.buf.format then
       vim.lsp.buf.format()
     elseif vim.lsp.buf.formatting then
